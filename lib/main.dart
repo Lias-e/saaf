7f4bff1925aed61824aace0adc5af2a0ac9b91c7
Ilyes Kouachi <ilyeskouachi@outlook.com>
2025-07-18 18:43:55 +0100

    auth

--- lib/main.dart
+++ lib/main.dart
@@ -1,9 +1,13 @@
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:saaf/auth/login_or_register.dart';
+import 'package:saaf/firebase_options.dart';
 import 'package:saaf/theme/dark_mode.dart';
 import 'package:saaf/theme/light_mode.dart';
 
-void main() {
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp(options: DefaultFirebaseOptions.currentPlatform);
   runApp(const SaafApp());
 }
 
